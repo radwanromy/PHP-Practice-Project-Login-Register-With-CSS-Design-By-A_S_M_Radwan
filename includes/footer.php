ba4b8e600de96bd968b60c655b975ca84d16ff36
A . S . M . RADWAN <50805604+radwanromy@users.noreply.github.com>
2021-06-01 02:23:26 +0600

    Php Project

---
+++ includes/footer.php
@@ -0,0 +1,6 @@
+
+</body>
+<footer>
+<p>This is my footer of the login and register form</p>
+</footer>
+</html>
---
+++ index.php
@@ -0,0 +1,18 @@
+
+<?php
+require_once 'includes/header.php';
+?>
+
+<?php
+    if (isset($_SESSION['sessionId'])) {
+        echo "You are logged in!";
+    } else {
+        echo "Home";
+        
+    }
+
+?>
+
+<?php
+require_once 'includes/footer.php';
+?>
